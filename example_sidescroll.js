4d98ce99d612c6a110c71c1358070502012d6a6c
Ari <ariannatnz@gmail.com>
2024-03-07 15:51:11 +1300

    side scrolling three little froggys

--- example_sidescroll.js
+++ example_sidescroll.js
@@ -1,35 +1,20 @@
 function draw_one_frame(cur_frac) {
-  let sun_size = height/8;
-
-  noStroke();
-  // sky
-  fill(100, 100, 214);
-  rect(0, 0, width, height);
-
-  // sun
-  fill(255, 255, 0);
-  ellipse(0.25 * width, 0.10 * height, sun_size);
-
-  // grass
-  fill(0, 200, 0);
-  rect(0, height/2, width, height/2);
-
   stroke(0);
+  angleMode(DEGREES);
+  rectMode(CENTER)
   fill(100, 100, 100);
 
   let b1_y = 0.55 * height;
-  let b2_y = 0.65 * height;
+  let b2_y = 0.85 * height;
+  let b3_y = 0.25 * height;
 
   let b1_size = height/12;
-  let b2_size = height/6;
 
   let grid_points1 = [
    -0.25 * width,
-    0.0 * width,
     0.25 * width,
-    0.50 * width,
     0.75 * width,
-    1.00 * width
+    1.25 * width
   ]
 
   if (debugView) {
@@ -37,7 +22,7 @@ function draw_one_frame(cur_frac) {
     strokeWeight(height/100);
     noFill();
     for(let i=0; i<grid_points1.length; i++) {
-      rect(grid_points1[i], b1_y, b1_size, 2*b1_size);
+      draw_froggy(grid_points1[i], b1_y, b1_size, true)
     }    
   }
 
@@ -45,22 +30,26 @@ function draw_one_frame(cur_frac) {
   noStroke();
   for(let i=0; i<grid_points1.length-1; i++) {
     let cur_x_pos = map(cur_frac, 0, 1, grid_points1[i], grid_points1[i+1])
-    rect(cur_x_pos, b1_y, b1_size, 2*b1_size);
+    draw_froggy(cur_x_pos, b1_y, b1_size, false)
   }
 
   let grid_points2 = [
-   -0.40 * width,
-    0.10 * width,
+    1.10 * width,
     0.60 * width,
-    1.10 * width
+    0.10 * width,
+    -0.40 * width
   ]
 
   if(debugView) {
     stroke(255, 0, 0);
     strokeWeight(height/100);
-    noFill();
+    noFill(); 
     for(let i=0; i<grid_points2.length; i++) {
-      rect(grid_points2[i], b2_y, b2_size, 2*b2_size);
+      push();
+      scale(-1, 1);
+      translate(-grid_points2[i], b2_y)
+      draw_froggy(0, 0, b1_size, true)
+      pop();
     }    
   }
 
@@ -68,7 +57,102 @@ function draw_one_frame(cur_frac) {
   noStroke();
   for(let i=0; i<grid_points2.length-1; i++) {
     let cur_x_pos = map(cur_frac, 0, 1, grid_points2[i], grid_points2[i+1])
-    rect(cur_x_pos, b2_y, b2_size, 2*b2_size);
+    push();
+    scale(-1, 1);
+    translate(-cur_x_pos, b2_y)
+    draw_froggy(0, 0, b1_size, false)
+    pop();
+  }
+
+  /// third oneeee
+  if(debugView) {
+    stroke(255, 0, 0);
+    strokeWeight(height/100);
+    noFill(); 
+    for(let i=0; i<grid_points2.length; i++) {
+      push();
+      scale(-1, 1);
+      translate(-grid_points2[i], b3_y)
+      draw_froggy(0, 0, b1_size, true)
+      pop();
+    }    
+  }
+
+  fill(100, 100, 100);
+  noStroke();
+  for(let i=0; i<grid_points2.length-1; i++) {
+    let cur_x_pos = map(cur_frac, 0, 1, grid_points2[i], grid_points2[i+1])
+    push();
+    scale(-1, 1);
+    translate(-cur_x_pos, b3_y)
+    draw_froggy(0, 0, b1_size, false)
+    pop();
   }
 }
 
+function draw_froggy(x, y, size, isDebug){
+
+  
+  if (isDebug){
+    stroke('red')
+    noFill();
+  } 
+  else {
+    
+    fill(44, 97, 24)
+    stroke('black')
+  }
+
+  // back foot leg
+  ellipse(x+10, y+25, size/2, size/6)
+  // front foot arm
+  // ellipse(x+12, y+27, size/3, size/6)
+
+  push();
+    translate(x,y)
+    rotate(-20)
+
+    // back leggy
+    ellipse(0, 15, size, size/2)
+
+    // back arm
+    ellipse(20, 15, size/4, size)
+
+    // body
+    ellipse(0, 0, size*2, size*1.1)
+  pop();
+
+  // front leggy
+  push();
+    translate(x,y)
+    rotate(-30)
+    ellipse(-30, -5, size, size/2)
+
+    // front arm
+    ellipse(20, 25, size/5, size)
+  pop();
+
+  // front foot leg 
+  ellipse(x-20, y+25, size/2, size/4)
+  // front foot arm
+  ellipse(x+42, y+27, size/3, size/6)
+
+  // head
+  ellipse(x+38, y-35, size/2, size/2.4) // back eye
+  ellipse(x+30, y-25, size/1.1, size/1.5)
+  
+  // eye stuff
+  ellipse(x+25, y-35, size/2, size/2.4)
+
+  fill('white')
+  ellipse(x+25, y-35, size/4, size/4)
+
+  fill('black')
+  circle(x+28, y-35, size/5)
+
+  //  smile
+  noFill()
+  arc(x+35, y-20, 15, 15, 0, 150, OPEN);
+}
+// rippl e- need more contrast - and has a beat to it - should keep it like that?
+// should do multiple different submissions with different experiments 
